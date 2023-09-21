47700edb701927bcde37113e81408782b176270e
Trung Tran Thien <trung.tranthien@elinext.com>
2023-09-22 01:09:55 +0700

    Update fixed eslint

--- src/pages/about.js
+++ src/pages/about.js
@@ -55,19 +55,19 @@ function about(props) {
                 Biography
               </h2>
               <p className="font-medium">
-                - Hi, I'm CodeBucks, a web developer and UI/UX designer with a
+                - Hi, I am CodeBucks, a web developer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. With 4 years of experience in the field. I
                 am always looking for new and innovative ways to bring my
-                clients' visions to life.
+                client visions to life.
               </p>
               <p className="my-4 font-medium">
                 - I believe that design is about more than just making things
-                look pretty – it's about solving problems and creating
+                look pretty – it is about solving problems and creating
                 intuitive, enjoyable experiences for users.
               </p>
               <p className="font-medium">
-                - Whether I'm working on a website, mobile app, or other digital
+                - Whether I am working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
